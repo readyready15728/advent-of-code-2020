e4bdb5bd70d3949b332bed5d5d41a288aa085f1e
Lynn Bradshaw <readyready15728@gmail.com>
2020-12-05 00:26:22 -0500

    Adding solution for day 5

---
+++ 05.rb
@@ -0,0 +1,25 @@
+codes = File.readlines('05.txt')
+seat_ids = []
+
+codes.each do |code|
+  row_code = code[0..6]
+  column_code = code[7..9]
+  row = 0
+  column = 0
+
+  row_code.each_char.each_with_index do |c, i|
+    if c == 'B'
+      row += 2**(6 - i)
+    end
+  end
+
+  column_code.each_char.each_with_index do |c, i|
+    if c == 'R'
+      column += 2**(2 - i)
+    end
+  end
+  
+  seat_ids.push 8 * row + column
+end
+
+puts seat_ids.max
